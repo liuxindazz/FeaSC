0bf91caf50a38e5de6e1494caab85290bf5482b7
liuxindazz <liuxinda@buaa.edu.cn>
2023-09-17 10:25:08 +0800

    Delete BYOL_FeaSC directory

--- BYOL_FeaSC/feasc/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    Feature-Suppressed Contrast for Self-Supervised Food Pre-training
-
-    31st ACM International Conference on Multimedia (ACM MM 2023)
-
-    The code is mainly based on https://github.com/facebookresearch/simsiam
-"""
--- BYOL_FeaSC/pretrain.sh
+++
@@ -1,5 +0,0 @@
-python main_pretrain.py \
-  -a resnet50 --gamma 0.4 --alpha 0.5\
-  -b 512 \
-  --dist-url 'tcp://localhost:10501' --multiprocessing-distributed --world-size 1 --rank 0 \
-  /dataset/Food2K/ > logs/BYOL_FeaSC.log  2>&1 &
